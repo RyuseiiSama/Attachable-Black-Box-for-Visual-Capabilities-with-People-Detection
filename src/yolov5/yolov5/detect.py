d3fac5ef7a49d98e42f3f54290adba35d9ad8353
ddq2 <hominhan56@gmail.com>
2024-06-24 15:03:54 +0800

    added yolov5

---
+++ src/yolov5/yolov5/detect.py
@@ -0,0 +1,315 @@
+# Ultralytics YOLOv5 🚀, AGPL-3.0 license
+"""
+Run YOLOv5 detection inference on images, videos, directories, globs, YouTube, webcam, streams, etc.
+
+Usage - sources:
+    $ python detect.py --weights yolov5s.pt --source 0                               # webcam
+                                                     img.jpg                         # image
+                                                     vid.mp4                         # video
+                                                     screen                          # screenshot
+                                                     path/                           # directory
+                                                     list.txt                        # list of images
+                                                     list.streams                    # list of streams
+                                                     'path/*.jpg'                    # glob
+                                                     'https://youtu.be/LNwODJXcvt4'  # YouTube
+                                                     'rtsp://example.com/media.mp4'  # RTSP, RTMP, HTTP stream
+
+Usage - formats:
+    $ python detect.py --weights yolov5s.pt                 # PyTorch
+                                 yolov5s.torchscript        # TorchScript
+                                 yolov5s.onnx               # ONNX Runtime or OpenCV DNN with --dnn
+                                 yolov5s_openvino_model     # OpenVINO
+                                 yolov5s.engine             # TensorRT
+                                 yolov5s.mlmodel            # CoreML (macOS-only)
+                                 yolov5s_saved_model        # TensorFlow SavedModel
+                                 yolov5s.pb                 # TensorFlow GraphDef
+                                 yolov5s.tflite             # TensorFlow Lite
+                                 yolov5s_edgetpu.tflite     # TensorFlow Edge TPU
+                                 yolov5s_paddle_model       # PaddlePaddle
+"""
+
+import argparse
+import csv
+import os
+import platform
+import sys
+from pathlib import Path
+
+
+
+import torch
+
+FILE = Path(__file__).resolve()
+ROOT = FILE.parents[0]  # YOLOv5 root directory
+if str(ROOT) not in sys.path:
+    sys.path.append(str(ROOT))  # add ROOT to PATH
+ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
+
+from ultralytics.utils.plotting import Annotator, colors, save_one_box
+
+from models.common import DetectMultiBackend
+from utils.dataloaders import IMG_FORMATS, VID_FORMATS, LoadImages, LoadScreenshots, LoadStreams
+from utils.general import (
+    LOGGER,
+    Profile,
+    check_file,
+    check_img_size,
+    check_imshow,
+    check_requirements,
+    colorstr,
+    cv2,
+    increment_path,
+    non_max_suppression,
+    print_args,
+    scale_boxes,
+    strip_optimizer,
+    xyxy2xywh,
+)
+from utils.torch_utils import select_device, smart_inference_mode
+
+#  ---------------------- ROS Imports -------------------------
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import Image as Image
+from std_msgs.msg import String
+from cv_bridge import CvBridge, CvBridgeError
+
+
+
+class Evaluator(Node):
+    def __init__(self):
+        super().__init__('evaluator')
+        self.subscription = self.create_subscription(
+            Image,
+            'camera/camera/color/image_raw',
+            self.evaluateee,
+            10)
+        self.publisher = self.create_publisher(String,'detections',10)
+        self.subscription  # prevent unused variable warning
+        self.webcam = False
+        # Directories
+        self.weights=ROOT / "yolov5s.pt"  # model path or triton URL
+        self.source=ROOT / "data/images"  # file/dir/URL/glob/screen/0(webcam)
+        self.data=ROOT / "data/coco128.yaml"  # dataset.yaml path
+        self.imgsz=(480, 640)  # inference size (height, width)
+        self.conf_thres=0.25  # confidence threshold
+        self.iou_thres=0.45  # NMS IOU threshold
+        self.max_det=1000  # maximum detections per image
+        self.device="0"  # cuda device, i.e. 0 or 0,1,2,3 or cpu
+        self.view_img=False  # show results
+        self.classes=None  # filter by class: --class 0, or --class 0 2 3
+        self.agnostic_nms=False  # class-agnostic NMS
+        self.augment=False  # augmented inference
+        self.visualize=False  # visualize features
+        self.line_thickness=3  # bounding box thickness (pixels)
+        self.hide_conf=False  # hide confidences
+        self.half=False  # use FP16 half-precision inference
+        self.dnn=False  # use OpenCV DNN for ONNX inference
+        self.source = str(self.source)
+
+        # Load model
+        self.device = select_device(self.device)
+        self.model = DetectMultiBackend(self.weights, device=self.device, dnn=self.dnn, data=self.data, fp16=self.half)
+        stride, self.names, self.pt = self.model.stride, self.model.names, self.model.pt
+        self.imgsz = check_img_size(self.imgsz, s=stride)  # check image size
+        # Dataloader
+        bs = 1  # batch_size
+        self.model.warmup(imgsz=(1 if self.pt or self.model.triton else self.bs, 3, *self.imgsz))  # warmup
+        self.seen, self.windows, self.dt = 0, [], (Profile(device=self.device), Profile(device=self.device), Profile(device=self.device))
+        print("Model Loaded, waiting for topic")
+
+
+
+    def evaluateee(self,msg):
+        img = msg
+        bridge = CvBridge()
+        try:
+            image = bridge.imgmsg_to_cv2(img,desired_encoding='passthrough')
+            print(image.shape)
+        except CvBridgeError as e:
+            print(e)
+        self.get_logger().info("Generating Results...")
+
+        im = image # Feed numpy image here
+        im0 = image
+        with self.dt[0]:
+            im = torch.from_numpy(im).to(self.model.device)
+            im = im.half() if self.model.fp16 else im.float()  # uint8 to fp16/32
+            im /= 255  # 0 - 255 to 0.0 - 1.0
+            if len(im.shape) == 3:
+                im = im[None]  # expand for batch dim
+            if self.model.xml and im.shape[0] > 1:
+                ims = torch.chunk(im, im.shape[0], 0)
+
+        # Inference
+        with self.dt[1]:
+            visualize = False
+            if self.model.xml and im.shape[0] > 1:
+                pred = None
+                for image in ims:
+                    if pred is None:
+                        pred = self.model(image, augment=self.augment, visualize=visualize).unsqueeze(0)
+                    else:
+                        pred = torch.cat((pred, self.model(image, augment=self.augment, visualize=visualize).unsqueeze(0)), dim=0)
+                pred = [pred, None]
+            else:
+                pred = self.model(im, augment=self.augment, visualize=visualize)
+        # NMS
+        with self.dt[2]:
+            pred = non_max_suppression(pred, self.conf_thres, self.iou_thres, self.classes, self.agnostic_nms, max_det=self.max_det)
+
+        # Second-stage classifier (optional)
+        # pred = utils.general.apply_classifier(pred, classifier_model, im, im0s)
+
+        # Process predictions
+        for i, det in enumerate(pred):  # per image
+            seen += 1
+            s += "%gx%g " % im.shape[2:]  # print string
+            gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+            annotator = Annotator(im0, line_width=self.line_thickness, example=str(self.names))
+            if len(det):
+                # Rescale boxes from img_size to im0 size
+                det[:, :4] = scale_boxes(im.shape[2:], det[:, :4], im0.shape).round()
+
+                # Print results
+                for c in det[:, 5].unique():
+                    n = (det[:, 5] == c).sum()  # detections per class
+                    s += f"{n} {self.names[int(c)]}{'s' * (n > 1)}, "  # add to string
+
+                # Write results
+                to_send = []
+                for *xyxy, conf, cls in reversed(det):
+                    c = int(cls)  # integer class
+                    label = self.names[c] if self.hide_conf else f"{self.names[c]}"
+                    confidence = float(conf)
+                    if c == 0:
+                        print(xyxy,conf,cls)   # xyxy is bbox loc, conf is confidence, cls is class. cls 0 is person
+                        to_send.append(str(xyxy))
+        send = String()
+        send.data = str(to_send)
+        self.publisher.publish(send)
+
+                
+
+        
+
+
+
+@smart_inference_mode()
+def run(
+    weights=ROOT / "yolov5s.pt",  # model path or triton URL
+    source=ROOT / "data/images",  # file/dir/URL/glob/screen/0(webcam)
+    data=ROOT / "data/coco128.yaml",  # dataset.yaml path
+    imgsz=(480, 640),  # inference size (height, width)
+    conf_thres=0.25,  # confidence threshold
+    iou_thres=0.45,  # NMS IOU threshold
+    max_det=1000,  # maximum detections per image
+    device="",  # cuda device, i.e. 0 or 0,1,2,3 or cpu
+    view_img=False,  # show results
+    classes=None,  # filter by class: --class 0, or --class 0 2 3
+    agnostic_nms=False,  # class-agnostic NMS
+    augment=False,  # augmented inference
+    visualize=False,  # visualize features
+    line_thickness=3,  # bounding box thickness (pixels)
+    hide_conf=False,  # hide confidences
+    half=False,  # use FP16 half-precision inference
+    dnn=False,  # use OpenCV DNN for ONNX inference
+):
+    
+
+    source = str(source)
+
+    webcam = False
+    # Directories
+ 
+
+    # Load model
+    device = select_device(device)
+    model = DetectMultiBackend(weights, device=device, dnn=dnn, data=data, fp16=half)
+    stride, names, pt = model.stride, model.names, model.pt
+    imgsz = check_img_size(imgsz, s=stride)  # check image size
+
+    # Dataloader
+    bs = 1  # batch_size
+    
+    dataset = None, None, None, None, None
+    # Run inference
+    model.warmup(imgsz=(1 if pt or model.triton else bs, 3, *imgsz))  # warmup
+    seen, windows, dt = 0, [], (Profile(device=device), Profile(device=device), Profile(device=device))
+
+
+    for path, im, im0s, vid_cap, s in dataset:
+        im = imm # Feed numpy image here
+        with dt[0]:
+            im = torch.from_numpy(im).to(model.device)
+            im = im.half() if model.fp16 else im.float()  # uint8 to fp16/32
+            im /= 255  # 0 - 255 to 0.0 - 1.0
+            if len(im.shape) == 3:
+                im = im[None]  # expand for batch dim
+            if model.xml and im.shape[0] > 1:
+                ims = torch.chunk(im, im.shape[0], 0)
+
+        # Inference
+        with dt[1]:
+            visualize = False
+            if model.xml and im.shape[0] > 1:
+                pred = None
+                for image in ims:
+                    if pred is None:
+                        pred = model(image, augment=augment, visualize=visualize).unsqueeze(0)
+                    else:
+                        pred = torch.cat((pred, model(image, augment=augment, visualize=visualize).unsqueeze(0)), dim=0)
+                pred = [pred, None]
+            else:
+                pred = model(im, augment=augment, visualize=visualize)
+        # NMS
+        with dt[2]:
+            pred = non_max_suppression(pred, conf_thres, iou_thres, classes, agnostic_nms, max_det=max_det)
+
+        # Second-stage classifier (optional)
+        # pred = utils.general.apply_classifier(pred, classifier_model, im, im0s)
+
+        # Process predictions
+        for i, det in enumerate(pred):  # per image
+            seen += 1
+            if webcam:  # batch_size >= 1
+                p, im0, frame = path[i], im0s[i].copy(), dataset.count
+                s += f"{i}: "
+            else:
+                p, im0, frame = path, im0s.copy(), getattr(dataset, "frame", 0)
+
+            p = Path(p)  # to Path
+            s += "%gx%g " % im.shape[2:]  # print string
+
+            if len(det):
+                # Rescale boxes from img_size to im0 size
+                det[:, :4] = scale_boxes(im.shape[2:], det[:, :4], im0.shape).round()
+
+                # Print results
+                for c in det[:, 5].unique():
+                    n = (det[:, 5] == c).sum()  # detections per class
+                    s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
+
+                # Write results
+                to_send = []
+                for *xyxy, conf, cls in reversed(det):
+                    c = int(cls)  # integer class
+                    label = names[c] if hide_conf else f"{names[c]}"
+                    confidence = float(conf)
+                    confidence_str = f"{confidence:.2f}"
+
+                    if c == 0:
+                        print(xyxy,conf,cls)   # xyxy is bbox loc, conf is confidence, cls is class. cls 0 is person
+
+
+def main():
+    rclpy.init(args=None)
+    evaluator = Evaluator()
+
+    rclpy.spin(evaluator)
+
+    evaluator.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
---
+++ src/yolov5/yolov5/interpreter.py
@@ -0,0 +1,165 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import Image as msg_Image
+from sensor_msgs.msg import CameraInfo
+from std_msgs.msg import String
+from cv_bridge import CvBridge, CvBridgeError
+import cv2
+import sys
+import os
+import numpy as np
+import pyrealsense2 as rs2
+
+
+def clean_str(string:str):
+   ''' cleans string of ',' and spaces on the ends '''
+   if string[0] == ',' or string[0] == ' ':
+      return clean_str(string[1:])
+   elif string[-1] == ',' or string[-1] == ' ':
+      return clean_str(string[:-1])
+   else:
+      return string
+class Interpreter(Node):
+    def __init__(self):
+      super().__init__('interpreter')
+      self.subscription = self.create_subscription(String,'detections',self.process_msg,10)
+      self.subscription 
+
+      # For distance calculation
+      self.bridge = CvBridge()
+      self.sub = self.create_subscription(msg_Image, 'camera/camera/depth/image_rect_raw', self.image_handler, 10)
+      self.sub_info = self.create_subscription(CameraInfo, 'camera/camera/depth/camera_info', self.imageDepthInfoCallback, 10)
+      self.color_img = self.create_subscription(msg_Image,'/camera/camera/color/image_raw',self.color,10)
+      self.publisher = self.create_publisher(msg_Image, 'labelled_img' ,10)
+      self.intrinsics = None
+      self.pix_grade = None
+      self.img = None
+      import pyrealsense2 as rs2
+      if (not hasattr(rs2, 'intrinsics')):
+        import pyrealsense2.pyrealsense2 as rs2
+      self.bboxes = []
+
+
+    def color(self,img):
+       self.get_logger().info("received an image")
+       self.img = img
+    
+    def process_msg(self,msg):
+      ''' Returns a list of all bounding boxes'''
+      unprocessed = msg.data
+
+      ## Processing ##
+      split = unprocessed.split(']')
+      for i in range(len(split)):
+        if split == []:
+           self.bboxes = []
+        
+        split[i] = split[i].replace('[','')
+        if split[i] == '':
+           continue
+        split[i] = clean_str(split[i])
+      while '' in split:
+         split.remove('')
+      bboxes = [list(map(int,x.split(','))) for x in split]
+      self.bboxes = bboxes
+    
+
+    def find_centre(self,bbox):
+       ''' Returns the centre of each bbox'''
+       i = bbox
+       x1 = i[0]
+       y1 = i[1]
+       x2 = i[2]
+       y2 = i[3]
+       centre = (int((x1+x2)/2),int((y1+y2)/2))
+       return centre
+
+    def image_handler(self,data):
+       
+       # If no detections
+       if self.bboxes == []:
+          print("Nothing Detected")
+          if not self.img:
+             return
+          self.publisher.publish(self.img)
+          return
+       
+       self.cimg = self.bridge.imgmsg_to_cv2(self.img,self.img.encoding)
+       self.dimg = self.bridge.imgmsg_to_cv2(data, data.encoding)
+       centres = []
+       for i in self.bboxes:
+          centres.append(self.find_centre(i))  # Get a list of centres for detected bounding box
+       depths = self.imageDepthCallback(self.dimg,centres)
+       for x in range(len(depths)):
+          # Write distance 
+          cv2.putText(self.cimg,f'd={round(depths[x],3)}m',(self.bboxes[x][0],self.bboxes[x][1]),cv2.FONT_HERSHEY_PLAIN,2,(0,0,0),2,cv2.LINE_AA)
+          # Draw lines from centre
+          cv2.line(self.cimg,(320,240),centres[x],color = (255,255,255))
+
+          # Draw Bounding box
+          cv2.rectangle(self.cimg, (self.bboxes[x][0], self.bboxes[x][1]), (self.bboxes[x][2],self.bboxes[x][3]), (255,255,255) , 1)
+       final_img = self.bridge.cv2_to_imgmsg(self.cimg,self.img.encoding)
+       self.publisher.publish(final_img)
+        
+       
+
+
+    def imageDepthCallback(self, cv_image, centres):
+        depths = []
+        for i in centres:
+          try:
+              # pick one pixel among all the pixels with the closest range:
+              # indices = np.array(np.where(cv_image == cv_image[cv_image > 0].min()))[:,0]
+              ################
+              pix = i
+              # line = '\rDepth at pixel(%3d, %3d): %7.1f(mm).' % (pix[0], pix[1], cv_image[pix[1], pix[0]])
+              print(cv_image.shape)
+              depth = cv_image[pix[1], pix[0]]/1000
+              depths.append(depth)
+              if self.intrinsics:
+                  result = rs2.rs2_deproject_pixel_to_point(self.intrinsics, [pix[0], pix[1]], depth)
+                  # line += '  Coordinate: %8.2f %8.2f %8.2f.' % (result[0], result[1], result[2])
+              if (not self.pix_grade is None):
+                  line += ' Grade: %2d' % self.pix_grade
+              # self.publisher.publish(msg)
+
+          except CvBridgeError as e:
+              print(e)
+              return
+          except ValueError as e:
+              return
+        print(depths)
+        return depths
+      
+    def imageDepthInfoCallback(self, cameraInfo):
+        try:
+            if self.intrinsics:
+                return
+            self.intrinsics = rs2.intrinsics()
+            self.intrinsics.width = cameraInfo.width
+            self.intrinsics.height = cameraInfo.height
+            self.intrinsics.ppx = cameraInfo.k[2]
+            self.intrinsics.ppy = cameraInfo.k[5]
+            self.intrinsics.fx = cameraInfo.k[0]
+            self.intrinsics.fy = cameraInfo.k[4]
+            if cameraInfo.distortion_model == 'plumb_bob':
+                self.intrinsics.model = rs2.distortion.brown_conrady
+            elif cameraInfo.distortion_model == 'equidistant':
+                self.intrinsics.model = rs2.distortion.kannala_brandt4
+            self.intrinsics.coeffs = [i for i in cameraInfo.d]
+        except CvBridgeError as e:
+            print(e)
+            return
+      
+      
+def main():
+    rclpy.init(args=None)
+    interpreter = Interpreter()
+    rclpy.spin(interpreter)
+
+
+if __name__ == '__main__':
+   main()
